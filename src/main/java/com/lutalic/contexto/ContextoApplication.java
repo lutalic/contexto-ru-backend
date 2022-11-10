23a7bd7ec10ca19cb5d0564112f55747e64cfbf0
ralerculte <ralerculte@yandex.ru>
2022-11-10 03:27:58 +0300

    Init LuContexto backend repo

---
+++ src/main/java/com/lutalic/contexto/ContextoApplication.java
@@ -0,0 +1,13 @@
+package com.lutalic.contexto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lutalic/contexto/ContextoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutalic.contexto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
